bad0cdce72767b037bf22fe91d6eaa822cab9e39
Niels Buekers <niels.buekers@gmail.com>
2022-05-21 21:46:07 +0200

    first attempt

--- contracts/Anvil.sol
+++ contracts/Anvil.sol
@@ -7,11 +7,19 @@ pragma solidity ^0.8.0;
 // We import this library to be able to use console.log
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 
 
 // This is the main building block for smart contracts.
-contract Anvil is ERC1155 {
+contract Anvil is ERC1155, VRFConsumerBaseV2 {
+
+    /**********
+    ** 
+    ** Anvil values
+    ** 
+    **********/
     uint256 public constant CROSSBOW_COMMON = 0;
     uint256 public constant CROSSBOW_UNCOMMON = 1;
     uint256 public constant CROSSBOW_RARE = 2;
@@ -31,30 +39,55 @@ contract Anvil is ERC1155 {
     uint256 public constant startingCommonCount = 10**27;
 
     //the probability for a successful upgrade
-    uint256 public constant uncommonUpgradeProbababilty = 0.5;
-    uint256 public constant rareUpgradeProbababilty  = 0.25;
-    uint256 public constant legendaryUpgradeProbababilty  = 0.05;
+    uint256 public constant UPGRADE_PERCENTAGE = 50;
+    //uint256 public constant uncommonUpgradeProbababilty = 0.5;
+    //uint256 public constant rareUpgradeProbababilty  = 0.25;
+    //uint256 public constant legendaryUpgradeProbababilty  = 0.05;
 
     //the amount of items of type {id} needed to attempt an upgrade to {id+1}.
     uint256 public constant UPGRADE_FACTOR = 10;
 
-    // An address type variable is used to store ethereum accounts.
     address public owner;
 
-    /**
-     * Contract initialization.
-     *
-     * The `constructor` is executed only once when the contract is created.
-     * The `public` modifier makes a function callable from outside the contract.
-     */
-    constructor() ERC1155("https://game.example/api/item/{id}.json") {
+
+    /**********
+    ** 
+    ** Chainlink values
+    ** 
+    **********/
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    bytes32 public keyHash;
+    uint256 public fee;
+    uint256 public _randomNumber;
+    uint256 public _requestId;
+    uint64 _subscriptionId;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 1;
+    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab; // Rinkeby
+    address linkTokenAddress = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;  // Rinkeby - not needed in V2?
+
+    constructor()   ERC1155("https://game.example/api/item/{id}.json")
+                    VRFConsumerBaseV2(vrfCoordinator){
+
+        /** CHAINLINK INIT **/
+        //not sure why not declare static above, tutorial does it like this
+        keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        _subscriptionId = 4720;
+        fee = 0.1*10**18; // fee of 0.1 LINK
+
         owner = msg.sender;
-        _mint(msg.sender, COMMON, startingCommonCount, "");
+
+        /** ERC1155 INIT **/
+        _mint(msg.sender, CROSSBOW_COMMON, startingCommonCount, "");
+        _mint(msg.sender, SWORD_COMMON, startingCommonCount, "");
+        _mint(msg.sender, DAGGER_COMMON, startingCommonCount, "");
     }
 
 
     /**
-     * @dev Destroys `UPGRADE_FACTOR` tokens of token type `id` from `from` and has `MINT_PERCENTAGE` chance of minting token `id+1`
+     * @dev Destroys `UPGRADE_FACTOR` tokens of token type `id` from `from` and has `UPGRADE_PERCENTAGE` chance of minting token `id+1`
      *
      * Emits a {???????? -- check if we need} event.
      *
@@ -86,4 +119,36 @@ contract Anvil is ERC1155 {
         _mint(msg.sender, newId, 1, "");
     }
 
+    //TODO check multiple - https://docs.chain.link/docs/chainlink-vrf-best-practices/#processing-simultaneous-vrf-requests
+    // Assumes the subscription is funded sufficiently.
+    function requestRandomWords() external onlyOwner {
+        // Will revert if subscription is not set and funded.
+
+        //TODO map requestId to the user + the upgrade he wants to do.
+        _requestId = COORDINATOR.requestRandomWords(
+        keyHash,
+        _subscriptionId,
+        requestConfirmations,
+        callbackGasLimit,
+        1 //numWords
+        );
+    }
+
+    //todo ensure requestId is mapped to user + upgrading request. atm only 1 for testing.
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        //number ends up between 1-100, used as percentage
+        _randomNumber = (randomWords[0] % 100) + 1;
+        console.log("random number is %s for requestId %s",_randomNumber, requestId);
+        if (_randomNumber <= UPGRADE_PERCENTAGE){
+            console.log("Yakshemash. upgraded your item!");
+        }
+        else{
+            console.log("sad story. Burnt your item.. Better luck next time");
+        }
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 }
